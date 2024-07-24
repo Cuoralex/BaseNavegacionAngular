cd81d61f4a0cef580503e78526d3a8cb1bf91c8f
Cuoralex <alexacuartas@gmail.com>
2024-07-24 15:37:26 -0500

    Generar navbar

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+
+// 1. Se agrega 
+// export class NavbarComponent implements OnInit {
+//      UserLoginOn:bolean=false;
+// constructor(){}
+//} 
+
+//2. Se debe hacer la importancion de OnInit en @angular/core
+
+// Sale un error ne NavbarComponent y debe hacerse la implementación de 
+// ngOnInit(): void {
+//  throw new Error('Method not implemented.');
+//}
+
+export class NavbarComponent implements OnInit {
+  userLoginOn:boolean=false;
+  constructor(){}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
